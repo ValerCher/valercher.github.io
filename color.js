02319778110710a83d767bd6bc7293383c785d1d
ValerCher <vtyslo@mail.ru>
2026-05-12 12:39:30 +0000

    Tue, May 12, 2026, 7:39 PM +07:00

---
+++ color.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function ()
+{
+    let body = document.querySelector('body');
+
+    document.querySelector('#green').addEventListener('click',
+         function(){body.style.backgroundColor='green';}
+        );
+
+    document.querySelector('#aqua').addEventListener('click',
+         function(){body.style.backgroundColor='aqua';}
+        );
+});
