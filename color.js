924b82ca9e3e88c57cc8297875d505fa27af80ce
ValerCher <vtyslo@mail.ru>
2026-05-12 16:08:34 +0000

    Tue, May 12, 2026, 11:08 PM +07:00

--- color.js
+++ color.js
@@ -12,11 +12,19 @@ document.addEventListener('DOMContentLoaded', function ()
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById(')
-
-
+    document.getElementById('index').onclick = function() {
+        window.location.href = 'index.html';
+    }
 
     document.getElementById('page2').onclick=function() {
         window.location.href = 'page2.html';
+    }
+
+    document.getElementById('page3').onclick=function() {
+        window.location.href = 'page3.html';
+    }
+
+    document.getElementById('page4').onclick=function() {
+        window.location.href = 'page4.html';
     }
 });
