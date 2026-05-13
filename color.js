ff41bc19e93a5116afa989b99dfaa7f580d18827
ValerCher <vtyslo@mail.ru>
2026-05-13 02:01:55 +0000

    Wed, May 13, 2026, 9:01 AM +07:00

--- color.js
+++ color.js
@@ -1,16 +1,3 @@
-document.addEventListener('DOMContentLoaded', function ()
-{
-    let body = document.querySelector('body');
-
-    document.querySelector('#green').addEventListener('click',
-         function(){body.style.backgroundColor='green';}
-        );
-
-    document.querySelector('#aqua').addEventListener('click',
-         function(){body.style.backgroundColor='aqua';}
-        );
-});
-
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('index').onclick = function() {
         window.location.href = 'index.html';
